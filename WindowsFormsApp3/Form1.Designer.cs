2ea272eba88935ec1b8781c0ce1e4fa432056876
sachinRameshGore <sachingore152@gmail.com>
2020-02-18 16:03:13 -0800

    second Commit

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.labelTime = new System.Windows.Forms.Label();
             this.btntime = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -71,11 +72,21 @@
             this.btntime.UseVisualStyleBackColor = true;
             this.btntime.Click += new System.EventHandler(this.btntime_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(502, 138);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btntime);
             this.Controls.Add(this.labelTime);
             this.Controls.Add(this.label2);
@@ -94,6 +105,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label labelTime;
         private System.Windows.Forms.Button btntime;
+        private System.Windows.Forms.Button button1;
     }
 }
 
